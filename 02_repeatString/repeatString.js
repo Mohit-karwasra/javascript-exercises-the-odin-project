19d8f2cfabcd0a64a0409c2974b3abfd0f0a8341
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-04-05 00:04:03 +0530

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+	if (num < 0) return "ERROR";
 
+	let resultant = "";
+	for (let i = 1; i <= num; i++) {
+		resultant += str;
+	}
+	return resultant;
 };
 
 // Do not edit below this line
