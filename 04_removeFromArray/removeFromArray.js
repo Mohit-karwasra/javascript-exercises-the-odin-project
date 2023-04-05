726d8b288c7f4808f5a667ca553117b3b1642513
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-04-05 09:54:23 +0530

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...elements) {
+	// for (let i = 0; i < elements.length; i++) {
+	// 	const index = arr.indexOf(elements[i]);
+	// 	if (index > -1) {
+	// 		arr.splice(index, 1);
+	// 	}
+	// }
+	// return arr;
 
+	return arr.filter((val) => !elements.includes(val));
 };
 
 // Do not edit below this line
