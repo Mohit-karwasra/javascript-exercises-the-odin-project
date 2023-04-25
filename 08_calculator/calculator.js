5b373c59335bdb337ccb48e4e92bac44fccbae8b
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-04-25 15:49:02 +0530

    Completed exercise 08-Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,48 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return Number(a) + Number(b);
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	const res = Number(a) - Number(b);
+	if (res < 0) {
+		res *= -1;
+	}
+	return res;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return Number(arr.reduce((acc, curr) => curr + acc, 0));
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	return arr.reduce((acc, curr) => curr * acc, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	while (b > 1) {
+		a *= 4;
+		b--;
+	}
+	return a;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+	if (a === 1) {
+		return 1;
+	}
+	let res = 1;
+	for (let i = a; i > 0; i--) {
+		res *= i;
+	}
+	return res;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
