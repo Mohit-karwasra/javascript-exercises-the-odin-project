6df0d0a9d8f85c9cd02c27b75b8e558d9ff297d8
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-04-26 11:53:56 +0530

    Done palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let replacedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+	return replacedStr.split("").reverse().join("") == replacedStr;
 };
 
 // Do not edit below this line
