a16026415e668cf2fa9f82ee70532c8b5b8fcc85
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-04-26 12:49:53 +0530

    Completed exercise-10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+	num = Number(num);
+	if (num < 0) {
+		return "OOPS";
+	}
+	if (num === 1 || num === 2) {
+		return 1;
+	}
+	let a = 1;
+	let b = 1;
+	for (let i = 3; i <= num; i++) {
+		let temp = b;
+		b = b + a;
+		a = temp;
+	}
+	return b;
 };
 
 // Do not edit below this line
