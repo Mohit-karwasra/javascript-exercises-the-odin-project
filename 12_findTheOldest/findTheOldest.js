ac916efc0f8486ff00d39807601105d9499d8e9e
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-04-26 16:47:27 +0530

    Done exercise-12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+	return arr.reduce((oldest, curr) => {
+		const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+		const currAge = getAge(curr.yearOfBirth, curr.yearOfDeath);
 
+		return oldestAge < currAge ? curr : oldest;
+	});
+};
+
+const getAge = function (birth, death) {
+	if (!death) {
+		death = new Date().getFullYear();
+	}
+	return death - birth;
 };
 
 // Do not edit below this line
